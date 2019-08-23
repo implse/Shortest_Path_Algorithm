4e738a277733d76469ece28df6a3ddeb28a488ad
Finite <finite.impulse@gmail.com>
2019-08-23 04:37:51 +0200

    Add Graph Vertices

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,6 +1,8 @@
 from heapq import heappop, heappush
+from Graph_Vertices import graph
 from math import inf
 
+# Time O(V+E log V)
 def dijkstras(graph, start):
     # Map vertices to their distance
     distances = {}
@@ -12,12 +14,12 @@ def dijkstras(graph, start):
     while vertices_to_explore:
         current_distance, current_vertex = heappop(vertices_to_explore)
 
-    for neighbor, edge_weight in graph[current_vertex]:
-        new_distance = current_distance + edge_weight
-        if new_distance < distances[neighbor]:
-            distances[neighbor] = new_distance
-            heappush(vertices_to_explore, (new_distance, neighbor))
+        for neighbor, edge_weight in graph[current_vertex]:
+            new_distance = current_distance + edge_weight
+            if new_distance < distances[neighbor]:
+                distances[neighbor] = new_distance
+                heappush(vertices_to_explore, (new_distance, neighbor))
     return distances
 
-distances_from_d = dijkstras(graph, 'D')
-print("\n\nShortest Distances: {0}".format(distances_from_d))
+distances_from_a = dijkstras(graph, 'A')
+print("\n\nShortest Distances: {0}".format(distances_from_a))
